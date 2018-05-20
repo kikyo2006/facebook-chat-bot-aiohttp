6e8d6e880a5524f67c4cce918d98df6542163dfa
Yu Kusanagi <yu.development@gmail.com>
2018-05-20 15:01:52 +0700

    update uwsgi

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ class BotControl(web.View):
             }
         })
         async with aiohttp.ClientSession as session:
-            await session.post("https://graph.facebook.com/v3.0/me/thread_settings", params=params, headers=headers, data=data)
+            async with session.post("https://graph.facebook.com/v3.0/me/thread_settings", params=params, headers=headers, data=data)
 
     async def send_message(self, sender_id, message_text):
 
@@ -81,7 +81,7 @@ class BotControl(web.View):
         })
 
         async with aiohttp.ClientSession as session:
-            await session.post("https://graph.facebook.com/v3.0/me/messages", params=params, headers=headers, data=data)
+            async with session.post("https://graph.facebook.com/v3.0/me/messages", params=params, headers=headers, data=data)
 
 
 
